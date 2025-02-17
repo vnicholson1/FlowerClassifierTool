7f677a7ce6f6d4c8aed451ccd87182d8ca433e70
Vincent Nicholson <vpnicholson21@gmail.com>
2025-02-17 13:17:16 +0000

    run experiments and save results to file

--- main.py
+++ main.py
@@ -31,6 +31,25 @@ def to_tiny_image(pillow_image):
     return np.array(smaller_image).flatten()
 
 
+def pretty_confusion_matrix(confusion_matrix, class_names, total):
+
+    num_correct = 0
+    for i in range(len(class_names)):
+        num_correct += confusion_matrix[i][i]
+
+    confusion_matrix.insert(0, [''] + class_names)
+    for i, row in enumerate(confusion_matrix[1:]):
+        row.insert(0, class_names[i])
+    s = [[str(e) for e in row] for row in confusion_matrix]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    text = '\n'.join(table)
+
+    text += '\n\n' + f'Accuracy = {(num_correct/total) * 100}%'
+    return text
+
+
 def main():
     # Create feature set
     class_name_and_paths = get_image_paths(use_train=True)
@@ -51,14 +70,16 @@ def main():
     class_name_and_paths = get_image_paths(use_train=False)
     class_names = [x.lower() for x in list(class_name_and_paths.keys())]
     # test it
-    confusion_matrix = np.zeros((len(class_names), len(class_names)))
+    confusion_matrix = np.zeros((len(class_names), len(class_names))).tolist()
     testing_data = {}
+    total_data = 0
     for class_name, paths in class_name_and_paths.items():
         testing_data[class_name] = []
         for path in paths:
             image = Image.open(path)
             image_array = to_tiny_image(image)
             testing_data[class_name].append(image_array)
+            total_data += 1
 
     print("Extracted features for the testing set")
     for class_name, list_of_image_features in testing_data.items():
@@ -67,8 +88,10 @@ def main():
             predicted, _ = knn.classify(image_features)
             confusion_matrix[class_names.index(predicted.lower())][class_names.index(class_name.lower())] += 1
 
-    print("Calculated accuracy and confusion matrix")
-    print(confusion_matrix)
+    results = pretty_confusion_matrix(confusion_matrix, class_names, total_data)
+    print(results)
+    with open('results.txt', 'w') as f:
+        f.write(results)
 
 if __name__ == '__main__':
     main()
