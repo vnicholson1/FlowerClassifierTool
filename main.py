2f9890164e9233cf0fab080fda2035fe907939f6
Vincent Nicholson <vpnicholson21@gmail.com>
2025-02-18 13:29:02 +0000

    test results for tiny images

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-from feature_extraction import to_tiny_image
+from feature_extraction import to_colour_histogram, to_tiny_image
 from nearest_neighbour import NearestNeighbourClassifier
 from utils import get_image_paths, pretty_confusion_matrix
 
@@ -51,8 +51,14 @@ def evaluate(function, *params):
     
 
 if __name__ == '__main__':
-    for image_size in [(2,2), (4,4), (8,8), (16,16), (32,32)]:
-        results = evaluate(to_tiny_image, image_size)
+    # for image_size in [(2,2), (4,4), (8,8), (16,16)]:
+    #     results = evaluate(to_tiny_image, image_size)
+    #     print(results)
+    #     with open(f'results_tiny_images_{image_size[0]}_{image_size[1]}.txt', 'w') as f:
+    #         f.write(results)
+
+    for number_of_bins in [5, 10, 20, 30, 40, 50]:
+        results = evaluate(to_colour_histogram, number_of_bins)
         print(results)
-        with open(f'results_tiny_images_{image_size[0]}_{image_size[1]}.txt', 'w') as f:
+        with open(f'results_color_hist_{number_of_bins}.txt', 'w') as f:
             f.write(results)
